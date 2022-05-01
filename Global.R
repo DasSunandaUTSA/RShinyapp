f61b81a34c7af933ee84b79245c89bd508700d0d
Sunanda Das UTSA <103918376+DasSunandaUTSA@users.noreply.github.com>
2022-05-01 00:35:02 -0500

    Add files via upload

---
+++ Global.R
@@ -0,0 +1,10 @@
+#read the restaurants data
+urlfile<-'https://raw.githubusercontent.com/DasSunandaUTSA/RShinyapp/main/combined_restaurants.csv'
+my_data <-read.csv(urlfile,stringsAsFactors = FALSE)
+
+
+ 
+
+
+  
+  
